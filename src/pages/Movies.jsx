9057a10dc493df375d1904b225288d03bec14304
Kunal72004 <143131261+Kunal72004@users.noreply.github.com>
2026-03-16 00:38:59 +0530

    fetching data on bases of rating

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -40,8 +40,25 @@ const Movies = () => {
     }
 
   return (
-    <div>
-      movies
+    <div className='min-h-screen bg-gray-200 py-8'>
+      <h1 className='text-3xl font-bold text-center mb-8'>Popular Movies</h1>
+      <div className=' max-w-4xl mx-auto flex gap-4 mb-8 px-4 items-center flex-col sm:flex-row'>
+        <input type="text" placeholder='Search Movies...' value={search} onChange={(e)=>setSerach(e.target.value)} className='w-full sm:flex-1 px-5 py-3 rounded-xl shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 transition' />
+        <select value={minRating} name="" id="" onChange={(e)=>setMinRating(e.target.value)} className='w-full  sm:w-48 border border-gray-300 px-5 py-3 rounded-xl shadow-md focus:outline-none focus:ring-2 focus:ring-yellow-500 transition'>
+          <option value="0">All Rating</option>
+          <option value="5">⭐ 5+</option>
+          <option value="6">⭐ 6+</option>
+          <option value="7">⭐ 7+</option>
+          <option value="8">⭐ 8+</option>
+        </select>
+      </div>
+      {filteredMovies.length === 0 ? (<p className='text-center text-gray-700 text-lg font-semibold'>No Movies Found 😅</p>):(
+        <div>
+          {filteredMovies.map((movie)=>(
+            <MovieCard key={movie.id} movie={movie}/>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
